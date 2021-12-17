6fc8305b933b9fff178688a53716ec5754dbf2c9
euqIrn3h <henrique11rodrigues@gmail.com>
2021-12-17 02:27:08 -0300

    Adicionando o arquivo de request na api da receita

---
+++ api.py
@@ -0,0 +1,13 @@
+import requests
+import json
+
+def consulta(cnpj):
+    url = f"https://www.receitaws.com.br/v1/cnpj/{cnpj}"
+    querrystring = {"tokem":"XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX","cnpj":"0690590000123","plugin":"RF"}
+    response = requests.request("GET",url,params=querrystring)
+
+    r = json.loads(response.text)
+
+    return r['nome'],r['logradouro'],r['numero'],r['complemento'],r['bairro'],r['municipio'],r['uf'],r['cep'],r['telefone'],r['email']
+
+    
